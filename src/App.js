6a3ccd02dff01e152f9d7e6840cd09ab7a7bfeed
Bartosz  Pietrzyk <bartosz.pietrzyk@radpoint.pl>
2023-02-05 17:44:18 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,66 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
-import Quagga from '@ericblade/quagga2';
-import { useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import { Select } from '@mantine/core';
 
 
 function App() {
+  const [data, setData] = useState("Not Found");
+  const [init, setInit] = useState(true);
+  const [cameras, setCameras] = useState([]);
+  const [camera, setCamera] = useState(null);
 
   useEffect(() => {
-    Quagga.init({
-      inputStream : {
-        name : "Live",
-        type : "LiveStream",
-        target: document.querySelector('#barcode-scanner')    // Or '#yourElement' (optional)
-      },
-      decoder : {
-        readers : [
-          "code_128_reader",
-          "ean_reader",
-          "ean_8_reader",
-          "code_39_reader",
-          "code_39_vin_reader",
-          "codabar_reader",
-          "upc_reader",
-          "upc_e_reader",
-          "i2of5_reader",
-          "2of5_reader",
-          "code_93_reader",
-        ],
-        debug: {
-          drawBoundingBox: false,
-          showFrequency: false,
-          drawScanline: true,
-          showPattern: false
-      }
-      }
-    }, function(err) {
-        if (err) {
-            console.log(err);
-            return
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-        Quagga.onDetected((data) => {
-          console.log(data.codeResult.code);
+    if(init) {
+      navigator.mediaDevices.enumerateDevices().then((deviceInfos) => {
+        console.log(deviceInfos);
+        const cameras = deviceInfos.filter((deviceInfos) => {
+          return deviceInfos.kind === 'videoinput';
         });
-    });
-  });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div id="barcode-scanner" width="300px" height="400px" border="">
+        const defaultCameraId = localStorage.getItem('defaultCameraId');
+        console.log(defaultCameraId);
+        setCameras(cameras);
+        if(defaultCameraId) {
+          const camera = cameras.find(camera => camera.deviceId === defaultCameraId);
+          setTimeout(() => {
+            setCamera(camera);
+          }, 10000)
+        }
+       
+      });
+      setInit(false);
+    }
+    
+  }, [init]);
 
-        </div>
-      </header>
+  const changeCamera = async () => {
+    const videos = document.getElementsByTagName('video');
+    const cameraVideo = videos[0];
+    console.log(cameraVideo, cameraVideo.srcObject)
+    if (cameraVideo && cameraVideo.srcObject) {
+      cameraVideo.srcObject.getTracks().forEach(t => t.stop());
+      const constraints = {
+        audio: false,
+        video: { deviceId: { exact: camera.deviceId } }
+      };
+      localStorage.setItem('defaultCameraId', camera.deviceId);
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      console.log(cameraVideo);
+      cameraVideo.srcObject = stream;
+    }
+    
+  }
+
+  const camerasDictionary = useMemo(() => {
+    return cameras.map((camera) => {
+      return {
+        value: camera,
+        label: camera.label,
+      }
+    })
+  }, [cameras])
+
+  useEffect(() => {
+    const handleCameraChange = async() => {
+      if(camera) {
+        changeCamera();
+      }
+    }
+    handleCameraChange();
+  }, [camera])
+
+
+  return (
+    
+    <div className="video-container">
+      <Select
+        label="Your favorite framework/library"
+        placeholder="Pick one"
+        data={camerasDictionary}
+        value={camera}
+        onChange={setCamera}
+      />
+
+      <BarcodeScannerComponent
+        id="asd"
+        width={500}
+        height={500}
+        // torch
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
+      
     </div>
   );
 }
-
-
 
 export default App;
